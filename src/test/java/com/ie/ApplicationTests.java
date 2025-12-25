23c04dbe0b45dc7ac8fc56f4bd300a9595447a3f
Zend <249420975@qq.com>
2025-12-25 08:41:07 +0800

    1111

---
+++ src/test/java/com/ie/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
